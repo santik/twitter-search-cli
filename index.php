a560afc690d3b20dce3cc4cb094506e7f00d1893
AF <AF>
2022-01-26 15:18:19 +0100

    save api data to the file

--- index.php
+++ index.php
@@ -12,25 +12,39 @@ $query = $argv[1];
 print_r("Search query: $query \n");
 
 print_r("Creating CSV \n");
-$header = ['first name', 'last name', 'email'];
+$header = [
+    'id',
+    'name',
+//    'location',
+//    'description',
+    'url'];
 //load the CSV document from a string
 $csv = Writer::createFromString();
 
 $csv->insertOne($header);
 
-$page = 1;
+$page = 0;
 do {
     print_r("Calling user search API page $page \n");
-    $res = $twitter->request('users/search', 'GET', ['q' => $query, "page" => $page]);
-    print_r("Search results: $res \n");
-    $res = [
-        [1, 2, 3],
-        ['foo', 'bar', 'baz'],
-        ['john', 'doe', 'john.doe@example.com'],
-    ];
-    $csv->insertAll($res);
+    $result = $twitter->request('users/search', 'GET', ['q' => $query, "page" => $page, 'include_entities' => false]);
+    $data = [];
+    foreach ($result as $account) {
+        $piece = [];
+        $piece[] = $account->id;
+        $piece[] = $account->name;
+//        $piece[] = $account->location;
+//        $piece[] = $account->description;
+        $piece[] = $account->url;
+        $data[] = $piece;
+    }
+
+    print_r("Search results: \n");
+    print_r($data);
+    print_r("\n");
+    $csv->insertAll($data);
     $page++;
-} while (count($res) != 0);
+} while (count($result) != 0 && $page < 52);
+
 
 $filename = "search_" . $query . "_" . time() . ".csv";
 print_r("Writing results to the file $filename \n");
